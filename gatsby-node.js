74853a6c9d29337de1dbf5e4a5bee27a34525a72
DavidDHansson <hansson.d.david@gmail.com>
2022-05-26 15:19:39 +0200

    Init with default gatsby starter

---
+++ gatsby-node.js
@@ -0,0 +1,12 @@
+exports.createPages = async ({ actions }) => {
+
+    const { createPage } = actions
+
+    createPage({
+        path: "/using-dsg",
+        component: require.resolve("./src/templates/using-dsg.js"),
+        context: {},
+        defer: true,
+    })
+    
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+const IndexPage = () => (
+    <Layout>
+        <Seo title="Home"/>
+        <Link to={"page-2"}>Page 2</Link> <br />
+        <Link to={"using-dsg"}>Using dsg</Link>
+    </Layout>
+)
+
+export default IndexPage
