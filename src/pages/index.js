8de4fa77d0ae048920fa27f73aa9f0fa45178c38
DavidDHansson <hansson.d.david@gmail.com>
2022-05-27 12:22:42 +0200

    Firebase base auth setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,33 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+import { SignInButton, LogOutButton } from "./../components/authButtons"
+import { auth } from "./../services/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const IndexPage = () => (
-    <Layout>
-        <Seo title="Home"/>
-        <Link to={"page-2"}>Page 2</Link> <br />
-        <Link to={"using-dsg"}>Using dsg</Link>
-    </Layout>
-)
+const IndexPage = () => {
+
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+      console.log(user);
+    }, [user])
+    
+    return (
+        <Layout>
+            <Seo title="Home" />
+            <Link to={"page-2"}>Page 2</Link> <br />
+            <Link to={"using-dsg"}>Using dsg</Link>
+
+            <br />
+            <div>
+                {(loading || error) ? <p>Loading...</p> : <SignInButton />}
+                {(loading || error) ? <p>Loading...</p> : <LogOutButton />}
+            </div>
+        </Layout>
+    );
+};
 
 export default IndexPage
---
+++ src/services/firebase.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, query, where, addDoc } from 'firebase/firestore/lite';
+import { GoogleAuthProvider, signInWithPopup, getAuth, signOut } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIE554LN8S2_OjMpVxBr5YMbFEZmdc5tc",
+    authDomain: "ux-and-web-exam-project.firebaseapp.com",
+    projectId: "ux-and-web-exam-project",
+    storageBucket: "ux-and-web-exam-project.appspot.com",
+    messagingSenderId: "607639095861",
+    appId: "1:607639095861:web:5304cc7d7b1cf270a13f7c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signIn = async () => {
+
+    const googleProvider = new GoogleAuthProvider();
+
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+
+        if (docs.docs.length === 0) {
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email
+                // TODO: add necesarry fields here for car rent
+            });
+        }
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const logOut = () => signOut(auth);
+
+export { signIn, logOut, auth };
