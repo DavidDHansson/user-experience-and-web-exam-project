451a7d1d2e69a879fe78beeb5306b1bf84d21400
DavidDHansson <hansson.d.david@gmail.com>
2022-05-29 14:46:58 +0200

    Setup bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,7 +26,15 @@ const Layout = ({ children }) => {
     return (
         <>
             <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-            <footer></footer>
+            
+            <div className="mb-3">
+                <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div className="mb-3">
+                <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
         </>
     )
 }
