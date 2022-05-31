6ca47209b687b030074758669c9448fb0d34d6de
laup <laup@itu.dk>
2022-05-31 14:57:04 +0200

    final final mommy tits

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import '../components/footerbar.css'
 
 const Footer = ({ siteTitle }) => (
     <div className="footer-bar-wrapper">
