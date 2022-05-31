5733cd611b77d0e31c9ee5a363762bd955c6e86a
DavidDHansson <hansson.d.david@gmail.com>
2022-05-31 17:12:52 +0200

    Work on map

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { memo, useEffect, useState } from 'react';
+import { GoogleMap, useJsApiLoader, TrafficLayer, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: "100%",
+    height: "600px",
+    marginTop: "-30px"
+};
+
+const center = {
+    lat: 55.6616636,
+    lng: 12.5913907
+};
+
+const Maps = () => {
+    const [markers, setMarkers] = useState([]);
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: "AIzaSyCO8ddpmWU35zCivbVfdaUE19AxIhb3lMo"
+    })
+
+    useEffect(() => {
+        if (isLoaded) {
+            setMarkers([{ lat: 55.6616636, lng: 12.7913907 }, { lat: 55.6616636, lng: 12.2913907 }, { lat: 55.9616636, lng: 12.5913907 }])
+        }
+    }, [isLoaded])
+
+    const didTapMarker = (index) => {
+        console.log(index);
+    }
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={14}
+        >
+            {markers.map((pos, index) => (
+                <Marker
+                    icon={"https://firebasestorage.googleapis.com/v0/b/ux-and-web-exam-project.appspot.com/o/Vector.png?alt=media&token=4edfb327-f1bd-4aaf-84c0-1c82aec9cbde"}
+                    position={pos}
+                    onClick={() => didTapMarker(index)}
+                />
+            ))}
+            {/* <TrafficLayer /> */}
+        </GoogleMap>
+    ) : <></>
+};
 
 
-const Maps = () => (
-  <>
-    <h1>works</h1>
-  </>
-)
-
-export default Maps;
+export default memo(Maps)
