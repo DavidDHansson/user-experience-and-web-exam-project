70c637c1325493e6954fbaf5d25f2ec3be0fce90
yovitus <vime@itu.dk>
2022-06-01 16:12:14 +0200

    no message

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,21 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import LogoBottom from '@icons/SKRRT2.svg'
+
 
 const Footer = ({ siteTitle }) => (
+    
     <div className="footer-bar-wrapper">
-        <h5>Footer</h5>
-    </div>
+        <div className="inner">
+            <h5>© 2022 SMB Inc.</h5>
+            <div className="mid-side">
+                <LogoBottom className="logo-footer" id="SKRRT"/>
+                <h5>For lyf</h5>    
+            </div>
+            <h5>Dansk</h5>
+        </div>
+    </div>  
 );
 
 // Footer.propTypes = {
--- src/pages/receipt.js
+++ src/pages/receipt.js
@@ -1,10 +1,49 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react"
 
 import Seo from "../components/seo"
 
 const Receipt = () => (
-    <div>
+    <div className="receiptpage">
         <Seo title="Receipt page" />
+        <div className="content">
+            <StaticImage src="../assets/images/flueben.png" className="checkmark"/>
+            <div className="title">
+                <h6>Kvittering</h6>
+                <h1>Tak, fordi du brugte <span className="gradient-text">Skrrt.</span></h1>
+            </div>
+            <div className="receipt">
+                <div className="line">
+                    <p className="left">Regnr.:</p>
+                    <p className="right">DK1234</p>
+                </div>
+
+                <div className="line">
+                    <p className="left">Minutpris:</p>
+                    <p className="right">DK1234</p>
+                </div>
+
+
+                <div className="line">
+                    <p className="left">Længde på tur:</p>
+                    <p className="right">DK1234</p>
+                </div>
+            <div className="seperator"></div>
+
+            <div className="line">
+                    <p className="left">betalt:</p>
+                    <p className="right">DK1234</p>
+                </div>
+
+                <div className="line">
+                    <p className="left">Din balance:</p>
+                    <p className="right">DK1234</p>
+                </div>
+
+            </div>
+            <a href="/" className="button secondary">Gå til forside </a>
+        </div>
+
         
     </div>
 )
