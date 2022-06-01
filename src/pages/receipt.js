6e5d32efb50520ac5ac7f4bcdcfab7f64361ccd5
DavidDHansson <hansson.d.david@gmail.com>
2022-06-01 22:30:33 +0200

    Correct price and data for receipt

--- src/pages/receipt.js
+++ src/pages/receipt.js
@@ -1,46 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import { getHistoryFromHistoryId } from "@services/firebase";
 
-const Receipt = () => (
-    <div className="receiptpage">
-        <div className="content">
-            <StaticImage src="../assets/images/flueben.png" className="checkmark" alt="checkmark"/>
-            <div className="title">
-                <h6>Kvittering</h6>
-                <h1>Tak, fordi du brugte <span className="gradient-text">Skrrt.</span></h1>
+const Receipt = () => {
+
+    const [receipt, setReceipt] = useState(null);
+
+    useEffect(() => {
+        const query = new URLSearchParams(window.location.search);
+        const receiptId = query.get("id");
+
+        if (receiptId) {
+            getHistoryFromHistoryId(receiptId)
+                .then(data => { setReceipt(data); console.log(data) });
+        }
+    }, []);
+
+    const getTimeLength = () => {
+        if (receipt) {
+            const startTime = receipt.startTime.toDate().getTime();
+            const endTime = receipt.endTime.toDate().getTime();
+            return Math.floor(((endTime - startTime) / 1000) / 60); 
+        } else {
+            return 0;
+        }
+    };
+
+    return (
+        <div className="receiptpage">
+            <div className="content">
+                <StaticImage src="../assets/images/flueben.png" className="checkmark" alt="checkmark" />
+                <div className="title">
+                    <h6>Kvittering</h6>
+                    <h1>Tak, fordi du brugte <span className="gradient-text">Skrrt.</span></h1>
+                </div>
+                <div className="receipt">
+                    <div className="line">
+                        <p className="left">Regnr.:</p>
+                        <p className="right">{receipt && receipt.id}</p>
+                    </div>
+
+                    <div className="line">
+                        <p className="left">Minutpris:</p>
+                        <p className="right">{receipt && receipt.priceMin} kr.</p>
+                    </div>
+
+
+                    <div className="line">
+                        <p className="left">Længde på tur:</p>
+                        <p className="right">{getTimeLength()} min</p>
+                    </div>
+                    <div className="seperator"></div>
+
+                    <div className="line">
+                        <p className="left">Betalt:</p>
+                        <p className="right">{receipt && receipt.price} kr.</p>
+                    </div>
+
+                    <div className="line">
+                        <p className="left">Din balance:</p>
+                        <p className="right">DK1234</p>
+                    </div>
+
+                </div>
+                <a href="/" className="button secondary">Gå til forside </a>
             </div>
-            <div className="receipt">
-                <div className="line">
-                    <p className="left">Regnr.:</p>
-                    <p className="right">DK1234</p>
-                </div>
-
-                <div className="line">
-                    <p className="left">Minutpris:</p>
-                    <p className="right">DK1234</p>
-                </div>
-
-
-                <div className="line">
-                    <p className="left">Længde på tur:</p>
-                    <p className="right">DK1234</p>
-                </div>
-                <div className="seperator"></div>
-
-                <div className="line">
-                    <p className="left">betalt:</p>
-                    <p className="right">DK1234</p>
-                </div>
-
-                <div className="line">
-                    <p className="left">Din balance:</p>
-                    <p className="right">DK1234</p>
-                </div>
-
-            </div>
-            <a href="/" className="button secondary">Gå til forside </a>
         </div>
-    </div>
-)
+    );
+};
 
 export default Receipt;
