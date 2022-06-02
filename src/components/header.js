2dcf3fd818da252748555a2e80a5fd00dfc3edfb
laup <laup@itu.dk>
2022-06-02 11:15:24 +0200

    bburgermenu

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { navigate } from "gatsby";
+import React, {useState} from "react";
+import { navigate, Link } from "gatsby";
 import { auth } from "@services/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import MenuIcon from '@icons/menu-icon.svg';
@@ -8,16 +8,24 @@ import UserIcon from '@icons/user-icon.svg';
 
 const Header = () => {
     const [user] = useAuthState(auth);
+    const [menuActive, setMenuActive] = useState(false);
+
 
     return (
         <div id="masthead">
             <nav role="navigation">
                 <div className="inner">
-                    <div><MenuIcon onClick={() => navigate("/")} role="menu" className="menu-icon" /></div>
+                    <div><MenuIcon onClick={() => setMenuActive(!menuActive)} role="menu" className="menu-icon" /></div>
                     <div><LogoIcon onClick={() => navigate("/")} role="home" className="logo" /></div>
                     <div className={user ? "profile-wrapper" : ""} onClick={() => navigate("/profile")} role="profile">{user ? <img src={user.photoURL} className="user-icon user-loggedin-icon" alt="user icon" /> : <UserIcon className="user-icon" />}</div>
                 </div>
             </nav>
+            <div className={`burger-menu ${menuActive ? 'active' : ''}`}>
+              <div className="exit" onClick={() => setMenuActive(!menuActive)}>X</div>
+              <Link to="/">Forside</Link>
+              <Link to="/guide">F.A.Q</Link>
+              <Link to="/profile">Min konto</Link>
+            </div>
         </div>
     );
 };
