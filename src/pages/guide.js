cb78bc556e1793227fc9e67fa9efa0e5ba593319
DavidDHansson <hansson.d.david@gmail.com>
2022-06-02 11:28:38 +0200

    Clean up

--- src/pages/guide.js
+++ src/pages/guide.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Header from "../components/header";
 
 const Guide = () => (
     <div className="guide-page">
@@ -9,13 +8,13 @@ const Guide = () => (
             <p>Du opretter en profil ved at logge ind på hjemmesiden med Google. Herefter vælger du, hvilken Google Acccount, du ønsker at benytte Skrrt med.</p>
             <h3>Hvordan lejer jeg en bil?</h3>
             <p>På kortet kan du se, hvilke biler, der er tilgængelige. Derefter trykker du på den bil, du ønsker at låne og starter din tur.</p>
-            
+
             <p>Når du er færdig med din tur parkeres bilen, og du afslutter turen.</p>
             <h3>Hvordan finder jeg ud af, hvilken bil, der er tættest på mig?</h3>
             <p>Tryk på "Find egen lokation" på kortet. Dette vil placere en markør på kortet, som viser din nuværende lokation. Herfra kan du se, hvilke biler, der er i dit nærområde.</p>
         </div>
     </div>
-    
+
 )
 
 export default Guide;
